aa005856db62dedf91d3ed8d1bbf6e4226ae015d
italianassassin3 <jose.frontanez@upr.edu>
2018-03-14 00:59:21 -0400

    Lab3Icom4035LL

---
+++ src/linkedLists/AbstractLinkedList.java
@@ -0,0 +1,12 @@
+package linkedLists;
+
+public class AbstractLinkedList<E> implements Cloneable {
+/**For the case specified in exercise 6 it's not possible to implement the code properly.
+ * The reason for this is because we are trying to implement a clone() for the different linked list with a very general and uniform code.
+ * This is contradictory since Double link list is implemented very differently from singly linked list and so on. 
+ * However there is a way to implement the clone() method for the several linked list. 
+ * The possible immediate solution is to implement a clone() method in each of the linked list class that we have and that we would like to have the method implemented in.
+ * 
+ * 
+ */
+}
